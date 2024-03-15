ed61d75f4de155151145c57642fa112cbf9e5c62
krizal1 <159939607+krizal1@users.noreply.github.com>
2024-03-15 19:29:13 +0300

    ADS3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,30 @@
 
 
 int cbinsearch(int *arr, int size, int value) {
+    int l = 0;
+    int r = size - 1;
     int count = 0;
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == value)
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == value) {
             count++;
+            int t = mid - 1;
+            while (t >= 0 && arr[t] == value) {
+                count++;
+                t--;
+            }
+            t = mid + 1;
+            while (t < size && arr[t] == value) {
+                count++;
+                t++;
+            }
+            return count;
+        } else if (arr[mid] < value)
+            l = mid + 1;
+        else
+            r = mid - 1;
     }
-    return 0;
+    return count;
 }
+
 
